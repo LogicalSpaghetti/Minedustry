4512e491de6b9c371daddfc8c0e77b02d992b8ce
Logical Spaghetti <130314555+LogicalSpaghetti@users.noreply.github.com>
2024-06-04 15:07:22 -0400

    SlotRandomizer done
    (tweak)

--- src/main/java/me/spaghetti/minedustry/block/helpers/SlotRandomizer.java
+++ src/main/java/me/spaghetti/minedustry/block/helpers/SlotRandomizer.java
@@ -3,12 +3,14 @@ package me.spaghetti.minedustry.block.helpers;
 import net.minecraft.util.math.Vec3i;
 import org.jetbrains.annotations.Range;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class SlotRandomizer {
     public static Vec3i[] getInventoryOffsets(@Range(from=1,to=Integer.MAX_VALUE)int size) {
         // the initial corner is the far North-West corner (negative, negative)
         Vec3i[] offsets = new Vec3i[size * 4];
         for (int i = 0; i < size; i++) {
-
             offsets[i] = new Vec3i(i, 0, -1); // north face
             offsets[i + size] = new Vec3i(size, 0, i); // east face
             offsets[i + 2 * size] = new Vec3i(size - 1 - i, 0, size); // south face
@@ -16,5 +18,26 @@ public class SlotRandomizer {
         }
 
         return offsets;
+    }
+
+    public static Vec3i[] getRandomOffsets(@Range(from=1,to=Integer.MAX_VALUE)int size) {
+        Random random = new Random();
+
+        Vec3i[] offsets = getInventoryOffsets(size);
+        Vec3i[] newOffsets = new Vec3i[offsets.length];
+
+        ArrayList<Integer> slots = new ArrayList<>();
+        for (int i = 0; i < offsets.length; i++) {
+            slots.add(i);
+        }
+
+        for(int i = 0; i < offsets.length; i++) {
+            int randomSlot = random.nextInt(slots.size());
+            int index = slots.get(randomSlot);
+            slots.remove(randomSlot);
+            newOffsets[i] = offsets[index];
+        }
+
+        return newOffsets;
     }
 }
